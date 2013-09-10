fcd2490e6eb14b2201a307bb843f253e363eb0cb
hml <icy.hmel@gmail.com>
2013-09-10 14:00:28 +0400

    initial commit

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,5 @@
+class GreetingsController < ApplicationController
+	def index
+		render :text => "<h1>Welcome to HELL<h1>"
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'f85edbc1dba550b1f1c360903d3d3080462c3ca561a792f62bd40385afd95aac3c3ded8eef14a2b06afda9f1d25b095716c91d3ba41f1d490a2f169527859d6d'
